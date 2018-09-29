19abbcf85f6f07c36191c2601823b9fa656e9aa1
Dexter Fryar <dexter.fryar@protonmail.com>
2018-09-29 14:43:08 -0500

    Added compiler env for NodeJS build Converted all paths to ~ convention

--- ts7600/build.sh
+++ ts7600/build.sh
@@ -2,21 +2,21 @@
 
 # COMPILE="yes"
 
-echo "Create epoch time file /root/epoch_time.txt"
-date +%s > /root/epoch_time.txt && EPOCH_TIME=$(cat /root/epoch_time.txt)
+echo "Create epoch time file ~/epoch_time.txt"
+date +%s > ~/epoch_time.txt && EPOCH_TIME=$(cat ~/epoch_time.txt)
 
 echo "Setup path for platform compiler"
-export PATH=$PATH:/root/.local/bin:/root/.local/arm-fsl-linux-gnueabi/bin
+sed 's/mesg n || true/PATH=$PATH:~\/.local\/arm-fsl-linux-gnueabi\/bin\n\nmesg n || true/' ~/.profile 
 
-echo "---> mkdir /root/Source /root/Download"
-mkdir -p /root/Source /root/Download /root/.local/bin
+echo "---> mkdir ~/Source ~/Download"
+mkdir -p ~/Source ~/Download ~/.local/bin
 
-echo "---> create /root/.netrc for aria2c"
-touch /root/.netrc && chmod 600 /root/.netrc
+echo "---> create ~/.netrc for aria2c"
+touch ~/.netrc && chmod 600 ~/.netrc
 
 # install compilers
-echo "---> cd /root/Download"
-cd /root/Download
+echo "---> cd ~/Download"
+cd ~/Download
 
 # Connection limit of 30 set on ftp.embeddedarm.com
 echo "---> Download SD card image ts4600_7600-mar092018-4GB.dd.bz2"
@@ -34,21 +34,21 @@ echo "---> Unarchive imx28-cross-glibc cross compiler"
 tar -xjf imx28-cross-glibc.tar.bz2
 
 echo "---> Move unarchived imx28-cross-glibc cross compiler to .local"
-mv arm-fsl-linux-gnueabi /root/.local/
+mv arm-fsl-linux-gnueabi ~/.local/
 
 # install kernel
 echo "---> Clone 2.6.35.3 kernel"
-cd /root/Source && git clone https://github.com/embeddedarm/linux-2.6.35.3-imx28.git
+cd ~/Source && git clone https://github.com/embeddedarm/linux-2.6.35.3-imx28.git
 
 echo "---> Clone 3.14.28 kernel"
-cd /root/Source && git clone https://github.com/embeddedarm/linux-3.14.28-imx28.git
+cd ~/Source && git clone https://github.com/embeddedarm/linux-3.14.28-imx28.git
 
 echo "---> Checkout linux-3.14.79-7600-4600"
 cd linux-3.14.28-imx28 && git checkout 7600-4600 && git checkout linux-3.14.79-7600-4600
 
 echo "---> Export compiler vars for platform"
 export ARCH=arm
-export CROSS_COMPILE=/root/.local/arm-fsl-linux-gnueabi/bin/arm-linux-
+export CROSS_COMPILE=~/.local/arm-fsl-linux-gnueabi/bin/arm-linux-
 export LOADADDR=0x40008000
  
 # This sets up the default configuration
@@ -72,5 +72,14 @@ fi
 # ./install_bootstream imx-bootlets-src-10.12.01/imx28_ivt_linux.sb mmcblk0 p1
 # ./install_hdr_mod mmcblk0p2
 
+echo "---> Clone NodeJS"
+cd ~/Source && git clone https://github.com/nodejs/node.git
+
+echo "---> cd to node source dir and copy .env for compiler"
+cd node && cp ~/Config/.env .
+
+echo "---> Source compiler env to build NodeJS"
+source .env
+
 echo "---> Keeping the container running with a tail of the build logs"
-tail -f /root/epoch_time.txt
+tail -f ~/epoch_time.txt
